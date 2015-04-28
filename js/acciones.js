d7d11b0bdfdbe6ee61ef38b4ea158f350fa359f5
AL40F13 <soriano11.ans@gmail.com>
2015-04-28 13:25:01 -0500

    dos
    barrer

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,22 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+	$('#btnvibrar').on('tap',
+	function(){
+		navigator.vibrate(1000);
+	});//tap a btnvibrar
+	$('#btnbeep').on('tap', 
+	function(){
+		navigator.beep(1);
+	});//tap a btnbeep
+	$('#izquierda').on('swipeleft',
+	function(){
+		alert("Barrio a la izquierda")
+	});//swipe left
+	$('#derecha').on ('swiperight',
+	function(){
+		alert("Barrio a la derecha")
+	});//swipe right
 }); 
 });
 
